56e03b15c85dc5d9364898d220c10e5249018f6f
Jesse Herrera <tuu07965@temple.edu>
2025-07-10 19:56:33 -0400

    Adding MVC and Index Page. Closes #2

---
+++ src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
@@ -0,0 +1,20 @@
+package guru.springframework.spring5recipeapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created on 7/10/2025 by Jesse H.
+ * Package: Controllers; Class: IndexController
+ * MVC for Index Page
+ */
+
+@Controller
+public class IndexController {
+
+    // Show the view using Thymeleaf template, and request mapping
+    @RequestMapping({"", "/", "index"})
+    public String getIndexPage() {
+        return "index";
+    } // End getIndexPage()
+} // End IndexController Class
