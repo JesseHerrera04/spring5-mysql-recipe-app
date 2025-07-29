55f0501a8c416a67868e127120197f4d667d954c
Jesse Herrera <tuu07965@temple.edu>
2025-07-28 21:54:47 -0400

    Created Unit Test with Mockito Mocks for Index Controller

---
+++ src/test/java/guru/springframework/spring5recipeapp/controllers/IndexControllerTest.java
@@ -0,0 +1,45 @@
+package guru.springframework.spring5recipeapp.controllers;
+
+import guru.springframework.spring5recipeapp.services.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created on 7/28/2025 by Jesse H.
+ * Package: Controllers (Test); Class: IndexControllerTest
+ * Unit Test for Model View Controller
+ */
+
+class IndexControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    IndexController controller;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        controller = new IndexController(recipeService);
+    }
+
+    @Test
+    void getIndexPage() {
+        String viewName = controller.getIndexPage(model);
+        assertEquals("index", viewName);
+        verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+    }
+}
